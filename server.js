6b9e08469cc81a6aa49b8745b03d29ed79d231da
Abhishek <s.abhishek7070@gmail.com>
2024-07-01 20:11:32 +0530

    modified

--- server.js
+++ server.js
@@ -31,6 +31,7 @@ db.once('open', () => {
 // Zod schema
 const contactSchema = z.object({
     name: z.string().min(1, 'Name is required'),
+    email:  z.string().email().optional().or(z.literal('')),
     phone: z.string().regex(/^\d+$/, { message: "Phone number should contain only digits" }).min(10, { message: "Phone number should be at least 10 digits" }),
     message: z.string().min(1, 'Message is required'),
 });
